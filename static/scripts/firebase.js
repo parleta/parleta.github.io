7ae2e32723895e2028bae4a41e07bdb0abb1e1de
megaspark12 <guyrave11@gmail.com>
2022-05-31 20:20:10 +0300

    14th commit

--- static/scripts/firebase.js
+++ static/scripts/firebase.js
@@ -1,33 +1,26 @@
-function setupFirebase() {
-    return new Promise((resolve, reject) => {
-        const xObj = new XMLHttpRequest();
-        xObj.overrideMimeType("application/json");
-        xObj.open('GET', 'https://parleta.github.io/static/scripts/config.json', true);
-        xObj.onreadystatechange = () => {
-            if (xObj.readyState == 4 && xObj.status == 200) {
-                // 2. call your callback function
-                const config = JSON.parse(xObj.responseText)
-                resolve(config)
-            }
-        };
-        xObj.send();
-    })
-    
-}
+//read the firebase info from json file
+const xObj = new XMLHttpRequest();
+xObj.overrideMimeType("application/json");
+xObj.open('GET', 'https://parleta.github.io/static/scripts/config.json', false);
+xObj.onreadystatechange = () => {
+    if (xObj.readyState == 4 && xObj.status == 200) {
+
+        //sets firebase up
+        const config = JSON.parse(xObj.responseText)
+        firebase.initializeApp(config);
+
+        // update firestore settings
+        firebase.firestore().settings({ timestampsInSnapshots: true });
+
+        //Signs the user out when closing the website
+        window.onunload = function() {
+            if(window.location.pathname == '/home.html')
+                firebase.auth().signOut()
+        }
+    }
+};
+xObj.send();
 
 
-window.onload = async () => {
 
-    const config = await setupFirebase()
 
-    firebase.initializeApp(config);
-
-    // update firestore settings
-    firebase.firestore().settings({ timestampsInSnapshots: true });
-
-    //Signs the user out when closing the website
-    window.onunload = function() {
-        if(window.location.pathname == '/home.html')
-            firebase.auth().signOut()
-    };
-}
