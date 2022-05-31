5eaf0f8a1cea894819be8447e23dc38534962f2e
megaspark12 <guyrave11@gmail.com>
2022-05-31 15:58:27 +0300

    11th commit

--- static/scripts/firebase.js
+++ static/scripts/firebase.js
@@ -1,33 +1,23 @@
-function getFirebaseConfig(){
-    return new Promise((resolve, reject) => {
-        const xObj = new XMLHttpRequest();
-        xObj.overrideMimeType("application/json");
-        xObj.open('GET', 'https://parleta.github.io/static/scripts/config.json', true);
-        xObj.onreadystatechange = () => {
-            if (xObj.readyState == 4 && xObj.status == 200) {
-                // 2. call your callback function
-                resolve(xObj.responseText);
-            }
+const xObj = new XMLHttpRequest();
+xObj.overrideMimeType("application/json");
+xObj.open('GET', 'https://parleta.github.io/static/scripts/config.json', true);
+xObj.onreadystatechange = () => {
+    if (xObj.readyState == 4 && xObj.status == 200) {
+        // 2. call your callback function
+        const config = JSON.parse(xObj.responseText)
+        firebase.initializeApp(JSON.parse(config));
+        // make auth and firestore references
+        auth = firebase.auth();
+        db = firebase.firestore();
+        storage = firebase.storage()
+
+        // update firestore settings
+        db.settings({ timestampsInSnapshots: true });
+        window.onunload = function() {
+            if(window.location.pathname == '/home.html')
+                auth.signOut().then(() => {});
         };
-        xObj.send();
-    })
-}
-
-let auth, db, storage;
-
-getFirebaseConfig()
-.then(config => {
-    firebase.initializeApp(JSON.parse(config));
-    // make auth and firestore references
-    auth = firebase.auth();
-    db = firebase.firestore();
-    storage = firebase.storage()
-
-    // update firestore settings
-    db.settings({ timestampsInSnapshots: true });
-    window.onunload = function() {
-        if(window.location.pathname == '/home.html')
-            auth.signOut().then(() => {});
-    };
-})
+    }
+};
+xObj.send();
 
