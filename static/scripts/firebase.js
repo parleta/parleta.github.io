c98cb5144564961302a2d2cb4e567076534e26c6
megaspark12 <guyrave11@gmail.com>
2022-05-31 15:39:34 +0300

    10th commit

--- static/scripts/firebase.js
+++ static/scripts/firebase.js
@@ -2,7 +2,7 @@ function getFirebaseConfig(){
     return new Promise((resolve, reject) => {
         const xObj = new XMLHttpRequest();
         xObj.overrideMimeType("application/json");
-        xObj.open('GET', './config.json', false);
+        xObj.open('GET', 'https://parleta.github.io/static/scripts/config.json', true);
         xObj.onreadystatechange = () => {
             if (xObj.readyState == 4 && xObj.status == 200) {
                 // 2. call your callback function
@@ -13,19 +13,21 @@ function getFirebaseConfig(){
     })
 }
 
+let auth, db, storage;
+
 getFirebaseConfig()
 .then(config => {
     firebase.initializeApp(JSON.parse(config));
+    // make auth and firestore references
+    auth = firebase.auth();
+    db = firebase.firestore();
+    storage = firebase.storage()
+
+    // update firestore settings
+    db.settings({ timestampsInSnapshots: true });
+    window.onunload = function() {
+        if(window.location.pathname == '/home.html')
+            auth.signOut().then(() => {});
+    };
 })
 
-// make auth and firestore references
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage()
-
-// update firestore settings
-db.settings({ timestampsInSnapshots: true });
-window.onunload = function() {
-    if(window.location.pathname == '/home.html')
-        auth.signOut().then(() => {});
-};
